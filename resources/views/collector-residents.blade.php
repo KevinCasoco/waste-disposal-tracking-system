c9adc93ab9a3a27bd21257da28c9bbc475129ca4
KevinCasoco <andreikevincasoco@gmail.com>
2023-11-19 21:37:05 +0800

    user residents delete

--- resources/views/collector-residents.blade.php
+++ resources/views/collector-residents.blade.php
@@ -99,7 +99,7 @@
                     </tbody>
                 </table>
 
-                <!-- Delete Modal -->
+            <!-- Delete Modal -->
             <div x-show="collectorResidentsDelete" class="fixed inset-0 overflow-y-auto flex items-center justify-center" x-cloak>
                 <div class="fixed inset-0 transition-opacity" aria-hidden="true">
                     <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
