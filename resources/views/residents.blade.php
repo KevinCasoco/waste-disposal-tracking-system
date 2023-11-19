97e2cd5e4119d77bfae7ff1693ec3a86a2fa56ee
KevinCasoco <andreikevincasoco@gmail.com>
2023-11-19 21:01:44 +0800

    residents delete with pop up modal hehe

--- resources/views/residents.blade.php
+++ resources/views/residents.blade.php
@@ -63,6 +63,7 @@
         <!--Card-->
         <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
 
+            <div x-data="{ residentsDelete: false }">
             <table id="example" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
                 <thead>
                     <tr>
@@ -83,20 +84,49 @@
                         <td class="text-sky-500 text-center">
                             <button id="" class="modal-open hover:border-indigo-900 text-blue-500 hover:text-indigo-900 font-bold py-2 px-4 rounded-full">Edit</button>
                         </td>
-                        {{-- <td class="text-red-500 text-center">Delete</td> --}}
                         <td>
-                            <!-- Delete button -->
-                            <form method="post" action="{{ route('residents.destroy', $item->id) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Delete</button>
-                            </form>
+                            <button @click="residentsDelete = true; itemToDelete = {{ $item->id }}">Delete</button>
                         </td>
                     </tr>
                     @endforeach
                 </tbody>
 
             </table>
+
+             <!-- Delete Modal -->
+             <div x-show="residentsDelete" class="fixed inset-0 overflow-y-auto flex items-center justify-center" x-cloak>
+                <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
+
+                <div x-show="residentsDelete" @click.away="residentsDelete = false"
+                    x-transition:enter="ease-out duration-300"
+                    x-transition:enter-start="opacity-0 transform scale-95"
+                    x-transition:enter-end="opacity-100 transform scale-100"
+                    x-transition:leave="ease-in duration-200"
+                    x-transition:leave-start="opacity-100 transform scale-100"
+                    x-transition:leave-end="opacity-0 transform scale-95"
+                    class="bg-white rounded-lg overflow-hidden transform transition-all sm:max-w-lg sm:w-full">
+                        <!-- ... (modal content) ... -->
+                        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-col sm:items-center">
+                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                            </svg>
+                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this user?</h3>
+                            <form x-data="{ itemToDelete: @json($item->id) }" method="post" :action="`{{ route('residents.destroy', '') }}/${itemToDelete}`">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit"
+                                        class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
+                                    Delete
+                                </button>
+                            </form>
+                            <button @click="residentsDelete = false"
+                                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                                Cancel
+                            </button>
+                        </div>
+                </div>
 
         </div>
         <!--/Card-->
