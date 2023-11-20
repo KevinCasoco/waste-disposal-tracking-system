6b1bc3f9e1c537978f1d291dbd0e0c976be82389
KevinCasoco <andreikevincasoco@gmail.com>
2023-11-20 10:48:55 +0800

    change the app logo icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Waste Management') }}</title>
 
+        <link rel="icon" type="image/x-icon" href="{{asset('/images/Waste-Logo.png')}}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
