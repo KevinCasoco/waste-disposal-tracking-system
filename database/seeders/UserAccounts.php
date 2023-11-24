a688ada06cb54411e6d41a117ca0eb5a5d073fcc
KevinCasoco <andreikevincasoco@gmail.com>
2023-11-24 21:47:57 +0800

    updated db seeders

--- database/seeders/UserAccounts.php
+++ database/seeders/UserAccounts.php
@@ -33,83 +33,12 @@ class UserAccounts extends Seeder
             'role' => 'admin',
         ]);
 
-        $admin = User::create([
-            'name' => $faker->name(),
-            'email' => 'admin@sample.com',
-            'password' => Hash::make('12345'),
-            'role' => 'admin',
-        ]);
-
-        $admin = User::create([
-            'name' => $faker->name(),
-            'email' => 'wastedisposal@gmail.com',
-            'password' => Hash::make('12345'),
-            'role' => 'admin',
-        ]);
-
-        $admin = User::create([
-            'name' => $faker->name(),
-            'email' => 'wastemanagement@sample.com',
-            'password' => Hash::make('12345'),
-            'role' => 'admin',
-        ]);
-
-        $admin = User::create([
-            'name' => $faker->name(),
-            'email' => 'wdtsofficial@gmail.com',
-            'password' => Hash::make('12345'),
-            'role' => 'admin',
-        ]);
-
         // seeding collector
         $collector = User::create([
             'name' => 'Jayar Grifaldea',
             'email' => 'jayargrifaldea@gmail.com',
             'password' => Hash::make('12345'),
             'role' => 'collector',
-        ]);
-
-        $collector = User::create([
-            'name' => $faker->name(),
-            'email' => 'collectors@gmail.com',
-            'password' => Hash::make('12345'),
-            'role' => 'collector',
-        ]);
-
-        $collector = User::create([
-            'name' => $faker->name(),
-            'email' => 'sample@hotmail.com',
-            'password' => Hash::make('12345'),
-            'role' => 'collector',
-        ]);
-
-        $collector = User::create([
-            'name' => $faker->name(),
-            'email' => 'garbage@gmail.com',
-            'password' => Hash::make('12345'),
-            'role' => 'collector',
-        ]);
-
-        $collector = User::create([
-            'name' => $faker->name(),
-            'email' => 'francis@gmail.com',
-            'password' => Hash::make('12345'),
-            'role' => 'collector',
-        ]);
-
-        $admin = User::create([
-            'name' => $faker->name(),
-            'email' => 'collector@gmail.com',
-            'password' => Hash::make('12345'),
-            'role' => 'admin',
-        ]);
-
-        // seeding residents
-        $residents = User::create([
-            'name' => 'Micaela Villamor',
-            'email' => 'mikayla@gmail.com',
-            'password' => Hash::make('12345'),
-            'role' => 'residents',
         ]);
 
         // seeding residents
@@ -120,13 +49,100 @@ class UserAccounts extends Seeder
             'role' => 'residents',
         ]);
 
-        // seeding residents
-        $residents = User::create([
-            'name' => 'Kevs 404',
-            'email' => 'kevs404official@gmail.com',
-            'password' => Hash::make('12345'),
-            'role' => 'residents',
-        ]);
+        // $admin = User::create([
+        //     'name' => $faker->name(),
+        //     'email' => 'wastemanagement@sample.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'admin',
+        // ]);
+
+        // $admin = User::create([
+        //     'name' => $faker->name(),
+        //     'email' => 'wdtsofficial@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'admin',
+        // ]);
+
+        // $admin = User::create([
+        //     'name' => $faker->name(),
+        //     'email' => 'wastemanagement@sample.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'admin',
+        // ]);
+
+        // $admin = User::create([
+        //     'name' => $faker->name(),
+        //     'email' => 'wdtsofficial@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'admin',
+        // ]);
+
+        // // seeding collector
+        // $collector = User::create([
+        //     'name' => 'Jayar Grifaldea',
+        //     'email' => 'jayargrifaldea@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'collector',
+        // ]);
+
+        // $collector = User::create([
+        //     'name' => $faker->name(),
+        //     'email' => 'collectors@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'collector',
+        // ]);
+
+        // $collector = User::create([
+        //     'name' => $faker->name(),
+        //     'email' => 'sample@hotmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'collector',
+        // ]);
+
+        // $collector = User::create([
+        //     'name' => $faker->name(),
+        //     'email' => 'garbage@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'collector',
+        // ]);
+
+        // $collector = User::create([
+        //     'name' => $faker->name(),
+        //     'email' => 'francis@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'collector',
+        // ]);
+
+        // $admin = User::create([
+        //     'name' => $faker->name(),
+        //     'email' => 'collector@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'admin',
+        // ]);
+
+        // // seeding residents
+        // $residents = User::create([
+        //     'name' => 'Micaela Villamor',
+        //     'email' => 'mikayla@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'residents',
+        // ]);
+
+        // // seeding residents
+        // $residents = User::create([
+        //     'name' => 'Shauna Loresca',
+        //     'email' => 'shaunaloresca@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'residents',
+        // ]);
+
+        // // seeding residents
+        // $residents = User::create([
+        //     'name' => 'Kevs 404',
+        //     'email' => 'kevs404official@gmail.com',
+        //     'password' => Hash::make('12345'),
+        //     'role' => 'residents',
+        // ]);
 
 
     }
