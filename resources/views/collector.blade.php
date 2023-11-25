47863b8e71778e4a85fbe0ff48bba7eaeabc39ec
KevinCasoco <andreikevincasoco@gmail.com>
2023-11-25 21:52:30 +0800

    admin collector edit  fetched data in modal fixed

--- resources/views/collector.blade.php
+++ resources/views/collector.blade.php
@@ -62,7 +62,8 @@
 
         <!--Card-->
         <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
-                <div x-data="{ deleteCollector: false, adminNewCollector: false, collectorEdit: false, itemToDelete: null, itemToEdit: null}">
+                {{-- <div x-data="{ deleteCollector: false, adminNewCollector: false, itemToDelete: null}"> --}}
+                    <div x-data="{ deleteCollector: false, adminNewCollector: false, collectorEdit: false, itemToDelete: null, itemToEdit: null}">
                     <button @click="adminNewCollector = true">Add New Collector</button>
                 <table id="example" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
                     <thead>
@@ -79,7 +80,7 @@
 
                     <tbody>
                         @foreach($data as $item)
-                        <tr>
+                        <tr x-on:click="itemToEdit = {{ $item->id }};">
                             <td>{{ $item->id }}</td>
                             <td>{{ $item->name }}</td>
                             <td>{{ $item->email }}</td>
@@ -206,11 +207,12 @@
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                             </svg> --}}
                             <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to update this user?</h3>
-                            {{-- @foreach($data as $item) --}}
+                            @foreach($data as $item)
+                            <div x-show="itemToEdit === {{ $item->id }}">
                             <form method="post" :action="`{{ route('collector.update_collector', '') }}/${itemToEdit}`">
                                 @csrf
                                 @method('patch')
-                                    {{-- <label for="name">ID:</label>
+                                    <label for="name">ID:</label>
                                     <input type="text" name="id" value="{{ $item->id }}" disabled>
                                     <br>
                                     <label for="name">Name:</label>
@@ -218,32 +220,21 @@
                                     <br>
                                     <label for="email">Email:</label>
                                     <input type="email" name="email" value="{{ $item->email }}" required>
-                                    <br> --}}
-                                    <label for="name">ID:</label>
-                                    <input type="text" name="id" value="" disabled>
                                     <br>
-                                    <label for="name">Name:</label>
-                                    <input type="text" name="name" value="" required>
-                                    <br>
-                                    <label for="email">Email:</label>
-                                    <input type="email" name="email" value="" required>
-                                    <br>
-                                    {{-- <label for="password">Password:</label>
-                                    <input type="password" name="password">
-                                    <br> --}}
-                                    {{-- <label for="role">Role:</label>
+                                    <label for="role">Role:</label>
                                     <select name="role" required>
                                         <option value="admin" {{ $item->role === 'admin' ? 'selected' : '' }}>Admin</option>
                                         <option value="collector" {{ $item->role === 'collector' ? 'selected' : '' }}>Collector</option>
                                         <option value="resident" {{ $item->role === 'resident' ? 'selected' : '' }}>Resident</option>
-                                    </select> --}}
+                                    </select>
                                     <br>
                                 <button type="submit"
                                         class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
                                     Update
                                 </button>
                             </form>
-                            {{-- @endforeach --}}
+                            </div>
+                            @endforeach
                             <button @click="collectorEdit = false"
                                     class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                                 Cancel
@@ -263,6 +254,15 @@
             // Set the itemToDelete value based on the clicked item's ID
             this.itemToDelete = itemId;
         }
+    </script>
+
+    <script>
+        window.addEventListener('DOMContentLoaded', () => {
+            Alpine.data('yourComponentName', () => ({
+                collectorEdit: false,
+                itemToEdit: null, // Variable to store the selected item
+            }));
+        });
     </script>
 
 	<script>
