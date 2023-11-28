bb8d95d177ed908d6c096eb88fff5c98a2e564b9
jaytzy024 <grifaldeajayar141@gmail.com>
2023-11-29 04:32:47 +0800

    Export-pdf-Datatable

--- resources/views/residents.blade.php
+++ resources/views/residents.blade.php
@@ -235,6 +235,48 @@
     </div>
     <!--/container-->
 
+    <!-- Datatable CSS -->
+    <link href='https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
+    <link href='https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css' rel='stylesheet' type='text/css'>
+
+    <style type="text/css">
+        .dt-buttons{
+        width: 86%;
+        position: relative;
+        margin-bottom: 10px;
+        
+
+        }
+        button.dt-button {
+            background-color: #22C55E;
+            color: white;
+            border: 1px solid;
+            border-radius: 11px;
+            height: 42px;
+
+            transition: background-color 0.3s ease; /* Add a smooth transition effect */
+
+        }
+
+        button.dt-button:hover {
+            background-color: #1c9d4b; /* Change the background color on hover */
+        }
+
+        </style>
+
+    <!-- jQuery Library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- Datatable JS -->
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    {{-- Export-Files-Buttons --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+
+    {{-- Alphine --}}
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
 
     <script>
@@ -244,7 +286,7 @@
         }
     </script>
 
-	<script>
+	{{-- <script>
 		$(document).ready(function() {
 
 			var table = $('#example').DataTable({
@@ -253,7 +295,34 @@
 				.columns.adjust()
 				.responsive.recalc();
 		});
-	</script>
+	</script> --}}
+
+    <!-- Script -->
+    <script>
+        $(document).ready(function() {
+            var empDataTable = $('#example').DataTable({
+                responsive: true,
+                dom: 'Blfrtip',
+                buttons: [
+                    {
+                        extend: 'copy',
+                    },
+                    {
+                        extend: 'pdf',
+                        exportOptions: {
+                            columns: [0, 1] // Column index which needs to export
+                        }
+                    },
+                    {
+                        extend: 'csv',
+                    },
+                    {
+                        extend: 'excel',
+                    }
+                ]
+            });
+        });
+    </script>
 
 @endif
 
