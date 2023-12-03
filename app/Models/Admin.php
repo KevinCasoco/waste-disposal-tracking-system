a648dcdfc4fd530843156cc6f8d2a9c7d31d0a33
KevinCasoco <andreikevincasoco@gmail.com>
2023-12-03 22:58:42 +0800

    created  new model, migration and seeders separate

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -20,11 +17,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'plate_no',
         'first_name',
         'last_name',
         'email',
-        'location',
         'password',
         'role',
         'status',
@@ -52,5 +47,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
 }
--- app/Models/Collector.php
+++ app/Models/Collector.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Collector extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -24,7 +21,6 @@ class User extends Authenticatable
         'first_name',
         'last_name',
         'email',
-        'location',
         'password',
         'role',
         'status',
@@ -52,5 +48,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
 }
