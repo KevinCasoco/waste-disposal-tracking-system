f891715ea6aa3738277b0d00744675fe58ea52bc
KevinCasoco <andreikevincasoco@gmail.com>
2023-12-06 15:36:49 +0800

    collector dashboard schedule

--- resources/views/add-collector.blade.php
+++ resources/views/add-collector.blade.php
@@ -1,3 +1,59 @@
+<x-app-layout>
+    @if (Auth::user()->role == 'collector')
+
+   <!-- START SIDEBAR -->
+   <div class="fixed left-0 top-0 w-60 h-full bg-white p-4">
+       <a class="flex item-center pb-4 border-b border-b-gray-700">
+           <img src="{{asset('/images/Waste-Logo.png')}}" alt="" class="w-16 h-16 rounded object-cover">
+           <span class="text-lg font-extrabold text-black ml-1">Waste Disposal Tracking System</span>
+       </a>
+
+       <ul class="mt-2" >
+        <li class="mb-1 group">
+            <a href="{{ asset('dashboard')}}" class="flex items-center py-2 px-4 text-black hover:bg-[#4ECE5D] hover:text-gray-100 rounded-md group-[.active]:bg-[#4ECE5D] group-[.active]:text-white group-[.selected]:bg-[#4ECE5D] group-[.selected]:text-white">
+                <i class="ri-dashboard-fill mr-3 text-lg"></i>
+                   <span class="text-sm">Dashboard</span>
+               </a>
+           </li>
+           <li class="mb-1 group">
+               <a href="{{ asset('admin')}}" class="flex items-center py-2 px-4 text-black hover:bg-[#4ECE5D] hover:text-gray-100 rounded-md group-[.active]:bg-[#4ECE5D] group-[.active]:text-white group-[.selected]:bg-[#4ECE5D] group-[.selected]:text-white">
+                   <i class="ri-admin-fill mr-3 text-lg"></i>
+                   <span class="text-sm">Admin</span>
+               </a>
+           </li>
+           <li class="mb-1 group">
+               <a href="{{ asset('collector')}}"  class="flex items-center py-2 px-4 text-black hover:bg-[#4ECE5D] hover:text-gray-100 rounded-md group-[.active]:bg-[#4ECE5D] group-[.active]:text-white group-[.selected]:bg-[#4ECE5D] group-[.selected]:text-white">
+                   <i class="ri-map-pin-user-fill mr-3 text-lg"></i>
+                   <span class="text-sm">Collector</span>
+               </a>
+           </li>
+           <li class="mb-1 group">
+               <a href="{{ asset('residents')}}" class="flex items-center py-2 px-4 text-black hover:bg-[#4ECE5D] hover:text-gray-100 rounded-md group-[.active]:bg-[#4ECE5D] group-[.active]:text-white group-[.selected]:bg-[#4ECE5D] group-[.selected]:text-white">
+                   <i class="ri-user-fill mr-3 text-lg"></i>
+                   <span class="text-sm">Residents</span>
+               </a>
+           </li>
+           <li class="mb-1 group active">
+            <a href="{{ asset('schedule')}}" class="flex items-center py-2 px-4 text-black hover:bg-[#4ECE5D] hover:text-gray-100 rounded-md group-[.active]:bg-[#4ECE5D] group-[.active]:text-white group-[.selected]:bg-[#4ECE5D] group-[.selected]:text-white">
+                    <i class="ri-calendar-2-fill mr-3 text-lg"></i>
+                   <span class="text-sm">Schedule</span>
+               </a>
+           </li>
+           {{-- <li class="mb-1 group">
+               <a href="Settings.html" class="flex items-center py-2 px-4 text-white hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
+                   <i class="ri-settings-2-line mr-3 text-lg"></i>
+                   <span class="text-sm">Settings</span>
+               </a>
+           </li>
+           <li class="mb-1 group">
+               <a href="Login.html" class="flex items-center py-2 px-4 text-white hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
+                   <i class="ri-logout-box-line mr-3 text-lg"></i>
+                   <span class="text-sm">Logout</span>
+               </a>
+           </li> --}}
+       </ul>
+   </div>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -38,7 +94,7 @@
 
     <form action="{{ route('collector-schedule.create_collector') }}" method="POST">
         @csrf
-        <label for='title'>{{ __('Title') }}</label>
+        <label for='title'>{{ __('Date of Collection') }}</label>
         <input type='text' class='form-control' id='title' name='title'>
 
         <label for="start">Start</label>
@@ -54,3 +110,7 @@
     </form>
 </body>
 </html>
+
+@endif
+
+</x-app-layout>
