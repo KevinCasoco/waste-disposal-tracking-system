26ef7c16401c0be55c6d473d3a159d298ca205f3
KevinCasoco <andreikevincasoco@gmail.com>
2023-12-08 14:27:16 +0800

    routing for create residents

--- routes/web.php
+++ routes/web.php
@@ -112,6 +112,7 @@ Route::middleware('auth', 'checkActiveStatus')->group(function () {
     Route::get('/collector', [CollectorController::class, 'index'])->name('collector');
     Route::post('/collector/create_collector', [AdminController::class, 'create_collector'])->name('collector.create_collector');
     Route::get('/residents', [UserController::class, 'index'])->name('residents');
+    Route::post('/admin/create_residents', [UserController::class, 'create_residents'])->name('admin.create_residents');
 
     // collection schedule
     Route::get('/schedule', [AdminController::class, 'showSchedule'])->name('schedule');
