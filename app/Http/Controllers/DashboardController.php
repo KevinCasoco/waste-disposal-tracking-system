b924009e88d2952c802699157af1f6aa91f85931
KevinCasoco <andreikevincasoco@gmail.com>
2023-12-10 15:59:29 +0800

    dashboard controller updated

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,55 +7,30 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    // public function countUsersByRole() {
-    //     $countAdmins = User::where('role', 'admin')->count();
-    //     $countCollector = User::where('role', 'collector')->count();
-    //     $countResidents = User::where('role', 'residents')->count();
-    //     $totalUser = User::count();
+    public function countUsersByRole()
+    {
+        $chartData = [
+            'admin' => [
+                'active' => User::where('role', 'admin')->where('status', 'active')->count(),
+                'inactive' => User::where('role', 'admin')->where('status', 'inactive')->count(),
+            ],
+            'collector' => [
+                'active' => User::where('role', 'collector')->where('status', 'active')->count(),
+                'inactive' => User::where('role', 'collector')->where('status', 'inactive')->count(),
+            ],
+            'residents' => [
+                'active' => User::where('role', 'residents')->where('status', 'active')->count(),
+                'inactive' => User::where('role', 'residents')->where('status', 'inactive')->count(),
+            ],
+        ];
 
-    //     return view('dashboard',
-    //         ['countAdmins' => $countAdmins,
-    //         'countCollector' => $countCollector,
-    //         'countResidents' => $countResidents,
-    //         'totalUser' => $totalUser,
-    //     ]);
-    // }
-//     public function countUsersByRole()
-// {
-//     $chart = User::all();
+            $chart = User::all();
 
-//     $countAdmins = User::where('role', 'admin')->count();
-//     $countCollector = User::where('role', 'collector')->count();
-//     $countResidents = User::where('role', 'residents')->count();
-//     $totalUser = User::count();
+            $countAdmins = User::where('role', 'admin')->count();
+            $countCollector = User::where('role', 'collector')->count();
+            $countResidents = User::where('role', 'residents')->count();
+            $totalUser = User::count();
 
-//     return view('dashboard', compact('chart', 'countAdmins', 'countCollector', 'countResidents', 'totalUser'));
-// }
-
-public function countUsersByRole()
-{
-    $chartData = [
-        'admin' => [
-            'active' => User::where('role', 'admin')->where('status', 'active')->count(),
-            'inactive' => User::where('role', 'admin')->where('status', 'inactive')->count(),
-        ],
-        'collector' => [
-            'active' => User::where('role', 'collector')->where('status', 'active')->count(),
-            'inactive' => User::where('role', 'collector')->where('status', 'inactive')->count(),
-        ],
-        'residents' => [
-            'active' => User::where('role', 'residents')->where('status', 'active')->count(),
-            'inactive' => User::where('role', 'residents')->where('status', 'inactive')->count(),
-        ],
-    ];
-
-    $chart = User::all();
-
-    $countAdmins = User::where('role', 'admin')->count();
-    $countCollector = User::where('role', 'collector')->count();
-    $countResidents = User::where('role', 'residents')->count();
-    $totalUser = User::count();
-
-    return view('dashboard', compact('chartData', 'chart', 'countAdmins', 'countCollector', 'countResidents', 'totalUser'));
-}
+        return view('dashboard', compact('chartData', 'chart', 'countAdmins', 'countCollector', 'countResidents', 'totalUser'));
+    }
 }
