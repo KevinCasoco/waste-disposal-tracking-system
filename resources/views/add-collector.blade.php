eb3bb35ed881c475b8bbacf8358a790c34d5a8bb
KevinCasoco <andreikevincasoco@gmail.com>
2023-12-10 15:42:02 +0800

    add new collector schedule updated

--- resources/views/add-collector.blade.php
+++ resources/views/add-collector.blade.php
@@ -5,8 +5,6 @@
 
         <!-- mobile menu bar -->
         <div class="bg-white text-black flex justify-end md:hidden">
-          <!-- logo -->
-          {{-- <a href="#" class="block p-4 text-black font-bold">WDT System</a> --}}
 
           <!-- mobile menu button -->
           <button class="mobile-menu-button p-4 focus:outline-none focus:bg-white">
@@ -19,17 +17,8 @@
         <!-- sidebar -->
         <div class="sidebar bg-white text-black w-64 space-y-6 py-1 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out z-20">
 
-          <!-- logo -->
-          {{-- <div class="ml-2 flex items-center rounded-md ">
-            <a class="flex item-center ">
-                <img src="{{ asset('/images/Waste-Logo.png') }}" alt="" class="w-14 h-14 rounded object-cover">
-                <span class="text-lg font-extrabold text-black ml-2">Waste Disposal Tracking System</span>
-            </a>
-          </div>
-          <hr class="my-2 text-gray-600"> --}}
-
-          <!-- nav -->
-          <nav>
+        <!-- nav -->
+        <nav>
        <ul class="mt-2" >
         <li class="mb-1 group">
             <a href="{{ asset('dashboard')}}" class="flex items-center py-2 px-4 text-black hover:bg-[#4ECE5D] hover:text-gray-100 rounded-md group-[.active]:bg-[#4ECE5D] group-[.active]:text-white group-[.selected]:bg-[#4ECE5D] group-[.selected]:text-white">
@@ -61,18 +50,6 @@
                    <span class="text-sm">Schedule</span>
                </a>
            </li>
-           {{-- <li class="mb-1 group">
-               <a href="Settings.html" class="flex items-center py-2 px-4 text-white hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                   <i class="ri-settings-2-line mr-3 text-lg"></i>
-                   <span class="text-sm">Settings</span>
-               </a>
-           </li>
-           <li class="mb-1 group">
-               <a href="Login.html" class="flex items-center py-2 px-4 text-white hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                   <i class="ri-logout-box-line mr-3 text-lg"></i>
-                   <span class="text-sm">Logout</span>
-               </a>
-           </li> --}}
            <li class="mb-1 group">
             <a href="{{ asset('profile') }}"
                 class="flex items-center py-2 px-4 text-black hover:bg-[#4ECE5D] hover:text-gray-100 rounded-md group-[.active]:bg-[#4ECE5D] group-[.active]:text-white group-[.selected]:bg-[#4ECE5D] group-[.selected]:text-white transition duration-200">
@@ -136,15 +113,14 @@
 
     <script>
         // grab everything we need
-    const btn = document.querySelector(".mobile-menu-button");
-    const sidebar = document.querySelector(".sidebar");
-    let isSidebarOpen = false;
+        const btn = document.querySelector(".mobile-menu-button");
+        const sidebar = document.querySelector(".sidebar");
+        let isSidebarOpen = false;
 
-    // add our event listener for the click
-    btn.addEventListener("click", () => {
-    sidebar.classList.toggle("-translate-x-full");
-    });
-
+        // add our event listener for the click
+        btn.addEventListener("click", () => {
+        sidebar.classList.toggle("-translate-x-full");
+        });
     </script>
 
     <form action="{{ route('collector-schedule.create_collector') }}" method="POST">
