a9ef2dbf5722baa38fe974f1c67d921ceff88e84
KevinCasoco <andreikevincasoco@gmail.com>
2023-12-27 14:31:06 +0800

    delete account in mobile view fixed

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,26 +6,6 @@
     </x-slot>
 
     <div class="bg-slate-200 flex justify-center items-center py-12">
-        {{-- <div class="grid grid-cols-1 md:cols-2 lg:grid-cols-2 gap-4 lg:gap-8 max-w-7xl mx-auto sm:px-6 lg:px-8 md:grid-cols-1 xl:grid-cols-4 xl:grid-rows-3 xl:grid-flow-col flex-grow-0">
-
-            <div class=" flex flex-col justify-start p-4 sm:p-8 sm:grid-cols-1 bg-white dark:bg-gray-800 shadow sm:rounded-lg  md:col-span-2 md:row-span-2">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
-            <div class=" flex flex-col justify-start p-4 sm:p-8 sm:grid-cols-1 bg-white dark:bg-gray-800 shadow sm:rounded-lg md:col-span-2 md:row-span-2">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="flex justify-end items-end flex-col bg-slate-200 col-span-2">
-                <div class="max-w-xl">
-                        @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div> --}}
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 lg:gap-8 max-w-7xl mx-auto sm:px-6 lg:px-8 md:grid-cols-1 xl:grid-cols-4 xl:grid-rows-3 xl:grid-flow-col flex-grow-0">
 
             <div class="flex flex-col justify-start p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg md:col-span-2 md:row-span-2">
@@ -41,7 +21,7 @@
             </div>
 
             <div class="flex justify-end">
-                <div class="max-w-xl ml-auto">
+                <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
