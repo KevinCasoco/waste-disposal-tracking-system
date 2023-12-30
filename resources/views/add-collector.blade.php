daffe11f9020858b18cf98f3d62d648a60dc1a3a
KevinCasoco <andreikevincasoco@gmail.com>
2023-12-30 11:59:06 +0800

    add collector dashboard updated

--- resources/views/add-collector.blade.php
+++ resources/views/add-collector.blade.php
@@ -45,7 +45,7 @@
                </a>
            </li>
            <li class="mb-1 group active">
-            <a href="{{ asset('schedule')}}" class="flex items-center py-2 px-4 text-black hover:bg-[#4ECE5D] hover:text-gray-100 rounded-md group-[.active]:bg-[#4ECE5D] group-[.active]:text-white group-[.selected]:bg-[#4ECE5D] group-[.selected]:text-white">
+            <a href="{{ asset('collector-schedule')}}" class="flex items-center py-2 px-4 text-black hover:bg-[#4ECE5D] hover:text-gray-100 rounded-md group-[.active]:bg-[#4ECE5D] group-[.active]:text-white group-[.selected]:bg-[#4ECE5D] group-[.selected]:text-white">
                     <i class="ri-calendar-2-fill mr-3 text-lg"></i>
                    <span class="text-sm">Schedule</span>
                </a>
