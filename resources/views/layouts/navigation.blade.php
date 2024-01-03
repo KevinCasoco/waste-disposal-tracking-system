a632e05792b319e6c6eef8902140a92712234600
KevinCasoco <andreikevincasoco@gmail.com>
2024-01-03 11:39:34 +0800

    navbar auth name with role

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -75,13 +75,12 @@
         </div> --}}
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+        <div class="pt-5 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
-                {{-- <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div> --}}
-                {{-- <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div> --}}
-                <div>{{ Auth::user()->first_name }} {{Auth::user()->last_name}}</div>
-            </div>
-{{--
+                <div class="font-medium text-base text-gray-600 dark:text-gray-200">
+                    {{ Auth::user()->first_name }} {{Auth::user()->last_name}} | {{ Auth::user()->role }}
+                </div>
+            {{--
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
