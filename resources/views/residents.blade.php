2756415aa5b7d2ca4ce476d3e796f8804a3efb54
KevinCasoco <andreikevincasoco@gmail.com>
2024-01-09 14:19:27 +0800

    residents data table make in maintainable

--- resources/views/residents.blade.php
+++ resources/views/residents.blade.php
@@ -408,49 +408,6 @@
         </div>
         <!--/container-->
 
-    <!-- Datatable CSS -->
-    <link href='https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
-    <link href='https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css' rel='stylesheet' type='text/css'>
-
-    <style type="text/css">
-        .dt-buttons{
-        width: 86%;
-        position: relative;
-        margin-bottom: 10px;
-
-
-        }
-        button.dt-button {
-            background-color: #22C55E;
-            color: white;
-            border: 1px solid;
-            border-radius: 11px;
-            height: 42px;
-
-            transition: background-color 0.3s ease; /* Add a smooth transition effect */
-
-        }
-
-        button.dt-button:hover {
-            background-color: #1c9d4b; /* Change the background color on hover */
-            color: black;
-        }
-
-        </style>
-
-    <!-- jQuery Library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-    <!-- Datatable JS -->
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
-    {{-- Export-Files-Buttons --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
-
     {{-- Alphine --}}
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
 
