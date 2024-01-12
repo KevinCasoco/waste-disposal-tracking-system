813f7094db2eaeb0b8880e40a007cd8601087df7
KevinCasoco <andreikevincasoco@gmail.com>
2024-01-12 16:40:13 +0800

    submit button updated for creating schedule of waste

--- resources/views/add-collector.blade.php
+++ resources/views/add-collector.blade.php
@@ -105,7 +105,7 @@
     <div class="form">
         <form action="{{ route('collector-schedule.create_collector') }}" method="POST">
             @csrf
-            <h1 class="mt-2 text-2xl font-bold text-center">Waste Collection Schedule</h1>
+            <h1 class="mt-6 text-2xl font-bold text-center">Waste Collection Schedule</h1>
 
             <label for='title'>{{ __('Location') }}</label>
             <select name="title" id='title' name='title' class="form-control" required>
@@ -135,7 +135,7 @@
             document.getElementById('time').value = currentTime;
             </script>
 
-            <input type="submit" value="Save" class="" />
+           <button type="submit" class="text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-full mt-6">Save</button>
         </form>
     </div>
 
--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -102,10 +102,10 @@
         </div>
     </div>
 
-    <div class="form">>
+    <div class="form">
         <form action="{{ route('schedule.create') }}" method="POST">
             @csrf
-            <h1 class="mt-2 text-2xl font-bold text-center">Waste Collection Schedule</h1>
+            <h1 class="mt-6 text-2xl font-bold text-center">Waste Collection Schedule</h1>
 
             <label for='title'>{{ __('Location') }}</label>
 
@@ -137,7 +137,7 @@
             document.getElementById('time').value = currentTime;
             </script>
 
-            <input type="submit" value="Save" class="btn btn-success" />
+            <button type="submit" class="text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-full mt-6">Save</button>
         </form>
     </div>
 
