8c6596d72d7a98701a1e2f89b1a4076ee35e97ac
KevinCasoco <andreikevincasoco@gmail.com>
2024-01-12 16:12:18 +0800

    logout modal for add schedule in collector

--- resources/views/add-collector.blade.php
+++ resources/views/add-collector.blade.php
@@ -58,20 +58,49 @@
             </a>
         </li>
         <li class="mb-1 group">
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-            <a :href="route('logout')"
-            onclick="event.preventDefault();
-                        this.closest('form').submit();"
+            <a onclick="document.getElementById('logoutModal').classList.remove('hidden')"
                 class="flex items-center py-2 px-4 text-black hover:bg-red-500 hover:text-gray-100 rounded-md group-[.active]:bg-red-500 group-[.active]:text-white group-[.selected]:bg-red500 group-[.selected]:text-white transition duration-200">
-                <i class="ri-logout-box-line mr-3 text-lg"></i>
+                    <i class="ri-logout-box-line mr-3 text-lg"></i>
                 <span class="text-sm">Logout</span>
             </a>
-        </form>
         </li>
        </ul>
        </nav>
    </div>
+
+    <!-- Logout Modal -->
+    <div id="logoutModal" class="hidden fixed inset-0 overflow-y-auto flex items-center justify-center z-30">
+        <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+
+        <div class="bg-white rounded-lg overflow-hidden transform transition-all flex justify-end p-2">
+            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-col">
+                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                </svg>
+                <h3 class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to logout?</h3>
+                <div class="flex justify-end items-end">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <a :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();"
+                            class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-3 py-3 text-center">
+                            <i class="ri-logout-box-line mr-1 text-lg"></i>
+                            <span class="text-sm">Logout</span>
+                        </a>
+                    </form>
+                    <div class="absolute mr-[100px] -mb-2.5">
+                        <button onclick="document.getElementById('logoutModal').classList.add('hidden')"
+                                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-3 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -83,7 +112,7 @@
 </head>
 <body>
     <style>
-        form {
+        .form {
             width: 100%;
             max-width: 500px;
             margin: 0 auto;
@@ -124,40 +153,42 @@
         });
     </script>
 
-    <form action="{{ route('collector-schedule.create_collector') }}" method="POST">
-        @csrf
-        <h1 class="mt-2 text-2xl font-bold text-center">Waste Collection Schedule</h1>
+    <div class="form">>
+        <form action="{{ route('collector-schedule.create_collector') }}" method="POST">
+            @csrf
+            <h1 class="mt-2 text-2xl font-bold text-center">Waste Collection Schedule</h1>
 
-        <label for='title'>{{ __('Location') }}</label>
-        <select name="title" id='title' name='title' class="form-control" required>
-            <option value="#">Location</option>
-            <option value="Barangay 168">Barangay 168</option>
-            <option value="Barangay 169">Barangay 169</option>
-            <option value="Barangay 170">Barangay 170</option>
-            <option value="Barangay 171">Barangay 171</option>
-            <option value="Barangay 172">Barangay 172</option>
-            <option value="Barangay 173">Barangay 173</option>
-            <option value="Barangay 174">Barangay 174</option>
-            <option value="Barangay 175">Barangay 175</option>
-            <option value="Barangay 176">Barangay 176</option>
-        </select>
+            <label for='title'>{{ __('Location') }}</label>
+            <select name="title" id='title' name='title' class="form-control" required>
+                <option value="#">Location</option>
+                <option value="Barangay 168">Barangay 168</option>
+                <option value="Barangay 169">Barangay 169</option>
+                <option value="Barangay 170">Barangay 170</option>
+                <option value="Barangay 171">Barangay 171</option>
+                <option value="Barangay 172">Barangay 172</option>
+                <option value="Barangay 173">Barangay 173</option>
+                <option value="Barangay 174">Barangay 174</option>
+                <option value="Barangay 175">Barangay 175</option>
+                <option value="Barangay 176">Barangay 176</option>
+            </select>
 
-        <label for="start">Date of Collection</label>
-        <input type='date' class='form-control' id='start' name='start' required value='{{ now()->toDateString() }}'>
+            <label for="start">Date of Collection</label>
+            <input type='date' class='form-control' id='start' name='start' required value='{{ now()->toDateString() }}'>
 
-        <label for="time">Time of Collection</label>
-        <input type="time" class='form-control' id="time" name="time" required>
+            <label for="time">Time of Collection</label>
+            <input type="time" class='form-control' id="time" name="time" required>
 
-        <script>
-          // Get the current time in the "HH:mm" format
-          var currentTime = new Date().toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute: '2-digit' });
+            <script>
+            // Get the current time in the "HH:mm" format
+            var currentTime = new Date().toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute: '2-digit' });
 
-          // Set the current time as the value for the "Time" input
-          document.getElementById('time').value = currentTime;
-        </script>
+            // Set the current time as the value for the "Time" input
+            document.getElementById('time').value = currentTime;
+            </script>
 
-        <input type="submit" value="Save" class="btn btn-success" />
-    </form>
+            <input type="submit" value="Save" class="btn btn-success" />
+        </form>
+    </div>
 </body>
 </html>
 
