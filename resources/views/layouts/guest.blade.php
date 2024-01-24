bd224e6a99cfc4ef1999dd611db9147c81504ea7
KevinCasoco <andreikevincasoco@gmail.com>
2024-01-24 14:20:16 +0800

    guest blade updated

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Waste Management') }}</title>
+
         <link rel="icon" type="image/x-icon" href="{{asset('/images/Waste-Logo.png')}}">
 
         <!-- Fonts -->
