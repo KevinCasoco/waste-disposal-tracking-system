2bd73f1a90c24a2156b540aadb19d1927ad2d15c
KevinCasoco <andreikevincasoco@gmail.com>
2024-01-25 10:14:52 +0800

    footer modified

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,3 @@
 {{-- <footer class="text-center py-4 bg-white text-gray-800">
-    <p class="text-sm">&copy; 2024 WDTS, CSD UCC North</p>
+    <p class="text-sm">&copy; 2024 WDTS, CSD UCC North BSIS</p>
 </footer> --}}
