51a5716e56a5830e0151bca269245724de7bc8c7
KevinCasoco <andreikevincasoco@gmail.com>
2024-01-27 11:43:07 +0800

    create button for collector

--- resources/views/collector-residents.blade.php
+++ resources/views/collector-residents.blade.php
@@ -107,7 +107,7 @@
             </div>
 
             <div x-data="{ collectorResidentsDelete: false, collectorEdit: false, collectorNewCollector: false, itemToDelete: null, itemToEdit: null }">
-                <div class="relative flex justify-end mb-2 ">
+                <div class="relative mb-16 ">
                     <button @click="collectorNewCollector = true" class="text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" > <i class="ri-add-circle-line mr-1"></i>Add New Collector</button>
                 </div>
 
