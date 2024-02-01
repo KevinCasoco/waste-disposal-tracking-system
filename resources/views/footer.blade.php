1745454c316d8ac94505e808bf365550ca1d1efa
KevinCasoco <andreikevincasoco@gmail.com>
2024-02-01 18:27:13 +0800

    footer updated

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,7 @@
 {{-- <footer class="text-center py-4 bg-white text-gray-800">
     <p class="text-sm">&copy; 2024 WDTS, CSD UCC North BSIS</p>
 </footer> --}}
+
+{{-- <footer class="text-center py-4 bg-white text-gray-800 fixed bottom-0 w-full">
+    <p class="text-sm">&copy; 2024 WDTS, CSD UCC North BSIS</p>
+</footer> --}}
