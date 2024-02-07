0e3582bb473fefc8d4610aa9fe3e6f02ba1b3871
KevinCasoco <andreikevincasoco@gmail.com>
2024-02-07 15:57:34 +0800

    residents table

--- resources/views/residents.blade.php
+++ resources/views/residents.blade.php
@@ -393,11 +393,10 @@
                                     </button>
 
                                     <div class="absolute mr-[93px]">
-                                        <button @click="residentsEdit = false"
-                                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                                            Cancel
-                                        </button>
-
+                                            <button @click.prevent="residentsEdit = false"
+                                                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                                                Cancel
+                                            </button>
                                     </div>
                                 </div>
                                 </div>
