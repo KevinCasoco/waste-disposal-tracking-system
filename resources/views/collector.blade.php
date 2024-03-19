a49301e571efcf9eae33d280daa61ae41f23d5dc
jaytzy024 <grifaldeajayar141@gmail.com>
2024-03-19 16:50:48 +0800

    schedule modal and cancel button in table

--- resources/views/collector.blade.php
+++ resources/views/collector.blade.php
@@ -325,7 +325,7 @@
                                                         Create
                                                     </button>
 
-                                                    <div class="md:hidden absolute mr-[90px]">
+                                                    <div class="absolute mr-[90px]">
                                                         <button @click="adminNewCollector = false"
                                                             class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                                                             Cancel
