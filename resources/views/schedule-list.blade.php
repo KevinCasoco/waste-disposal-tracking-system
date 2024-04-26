ee65944b9ab72227ea4358a91b82a8c7ed072d6d
KevinCasoco <andreikevincasoco@gmail.com>
2024-04-26 15:15:43 +0800

    refresh and filter modified color

--- resources/views/schedule-list.blade.php
+++ resources/views/schedule-list.blade.php
@@ -156,11 +156,11 @@
                                     </div>
                                     <div class="flex justify-end gap-2 mb-2 mt-6 md:mt-6">
                                         <a href="{{ asset('schedule-list') }}"
-                                            class="btn text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center">
+                                            class="btn text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center">
                                             Refresh
                                         </a>
                                         <button type="submit"
-                                            class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center">
+                                            class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-4 py-2 text-center">
                                             Filter
                                         </button>
                                     </div>
