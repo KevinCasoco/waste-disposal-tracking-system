be32a4474b6fa50a6fe113d38645abe564916f3f
KevinCasoco <andreikevincasoco@gmail.com>
2024-04-27 18:23:33 +0800

    admin and residents wdts logo in google maps marker

--- resources/views/residents-trash-bin.blade.php
+++ resources/views/residents-trash-bin.blade.php
@@ -30,10 +30,15 @@
                 });
 
                 @foreach($trashBins as $trashBin)
+                var iconSize = new google.maps.Size(32, 32); // Adjust the size as needed
                 var marker = new google.maps.Marker({
                     position: { lat: {{ $trashBin->latitude }}, lng: {{ $trashBin->longitude }} },
                     map: map,
-                    title: "Trash Bin Location"
+                    title: "Trash Bin Location",
+                    icon: {
+                        url: '{{ asset("/images/Waste-Logo.png") }}',
+                        scaledSize: iconSize
+                    }
                 });
                 @endforeach
 
